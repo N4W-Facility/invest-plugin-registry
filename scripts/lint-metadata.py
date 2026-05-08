8bf07909501731b7a801b438898aa34af96f978d
James Douglass <jdouglass@stanford.edu>
2026-05-08 13:59:10 -0700

    Adding linting for pyproject.toml and plugin.json

--- scripts/lint-metadata.py
+++ scripts/lint-metadata.py
@@ -6,10 +6,16 @@ Tests performed:
 
 """
 import argparse
+import json
+import re
 import tomllib
 
 import requests
 import validate_pyproject
+
+ALLOWED_PLUGIN_TYPES = [
+    'preprocessing', 'postprocessing', 'workflow', 'model_variant',
+    'new_model', 'other']
 
 
 def _validate_pyproject_file(filepath):
@@ -39,7 +45,54 @@ def _validate_pyproject_file(filepath):
 
 
 def _valdiate_project_json_file(filepath):
-    pass
+    try:
+        with open(filepath, 'r') as project_json:
+            json_data = json.load(project_json)
+    except json.decoder.JSONDecodeError as error:
+        return f"Could not parse JSON file at {filepath}: {str(error)}"
+
+    # We're assuming that the top-level object is an array
+    issues = []
+    for object_num, plugin_data in enumerate(json_data):
+        if "repo_url" in plugin_data:
+            url = plugin_data['repo_url']
+            req = requests.head(url)
+            if not req.ok:
+                issues.append(f"URL {url} could not be found")
+        else:
+            issues.append(
+                f"Plugin {object_num} is missing the attribute 'repo_url'")
+
+        if "version" in plugin_data:
+            if not re.match('^[0-9]+\.[0-9]+\.[0-9+$',
+                            str(plugin_data['version'])):
+                issues.append(
+                    "Version string must be in the form MAJOR.MINOR.PATCH")
+        else:
+            issues.append(
+                f"Plugin {object_num} is missing the attribute 'version'")
+
+        if 'plugin_type' in plugin_data:
+            plugin_type = plugin_data['plugin_type']
+            if plugin_type not in ALLOWED_PLUGIN_TYPES:
+                issues.append(
+                    f"Plugin type '{plugin_type}' must be one of the "
+                    f'supported plugin types {ALLOWED_PLUGIN_TYPES}')
+        else:
+            issues.append(
+                f"Plugin {object_num} is missing the attribute 'plugin_type'")
+
+        # We don't have any requirements about keywords at this time, so
+        # skipping any validation except for existence of the attribute.
+        if 'keywords' not in plugin_data:
+            issues.append(
+                f"Plugin {object_num} is missing the attribute 'keywords'")
+
+    if issues:
+        return (
+            f"{filepath} was found to have some formatting issues:"
+            + "\n".join(f"* {issue}" for issue in issues))
+    return None
 
 
 def main(args=None):
