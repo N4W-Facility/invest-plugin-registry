f0413234c915b032124612eedf3e45a56b0e38f7
Megan Nissel <megnissel@gmail.com>
2026-06-09 11:22:47 -0400

    derive ALLOWED_PLUGIN_TYPES from the same PLUGIN_TYPES dict used in other scripts

--- scripts/lint-metadata.py
+++ scripts/lint-metadata.py
@@ -24,12 +24,13 @@ import tomllib
 import requests
 import validate_pyproject.api  # requires packaging>=24.2 for enforcement.
 
+from utils import PLUGIN_TYPES
+
+
 logging.basicConfig(level=logging.DEBUG)
 LOGGER = logging.getLogger(__name__)
 
-ALLOWED_PLUGIN_TYPES = [
-    'preprocessing', 'postprocessing', 'workflow', 'invest_model_variant',
-    'new_model', 'other']
+ALLOWED_PLUGIN_TYPES = PLUGIN_TYPES.keys()
 
 Required = collections.namedtuple('Required', ['tomlattr'])
 Optional = collections.namedtuple('Optional', ['tomlattr'])
@@ -175,7 +176,7 @@ def _validate_project_json_file(filepath):
             if plugin_type not in ALLOWED_PLUGIN_TYPES:
                 issues.append(
                     f"Plugin type '{plugin_type}' must be one of the "
-                    f'supported plugin types {ALLOWED_PLUGIN_TYPES}')
+                    f'supported plugin types {list(ALLOWED_PLUGIN_TYPES)}')
         else:
             issues.append(
                 f"Plugin {object_num} is missing the attribute 'plugin_type'")
